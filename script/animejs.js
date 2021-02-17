f0db0fa1fe5f16314b7bad10a5e1c036a63f99c6
Aurélien <aureliendu3809@outlook.fr>
2021-02-17 09:03:55 +0100

    none

--- script/animejs.js
+++ script/animejs.js
@@ -3,10 +3,10 @@ fetch(`https://api.jikan.moe/v3/anime/${id}`).then((response) => {
     return response.json();
 }).then((anime) =>{
     console.log(anime);
-    let animeDiv = document.querySelector('.anime');
+    let imageDiv = document.querySelector('#image_url');
     let image = document.createElement('img');
     image.setAttribute("src", `${anime.image_url}`)
     document.querySelector('#title').innerHTML = anime.title;
-    animeDiv.prepend(image);
+    imageDiv.appendChild(image);
     
 });  
