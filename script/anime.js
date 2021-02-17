3aefaba1f99ec91d6318a20b1d46d659bfcce248
Aurélien <aureliendu3809@outlook.fr>
2021-02-17 21:19:53 +0100

    pret pour merge (besoin du constructor)

--- script/anime.js
+++ script/anime.js
@@ -22,59 +22,55 @@ document.querySelector('.aside-menu').addEventListener('click', (el) => {
     el = el.target;
     if(el.dataset.genre){
         console.log('nnan');
-        
-        if(document.querySelector(`#${el.dataset.genre}`) === null){
-            document.querySelector('.main').appendChild(Card.createCard());
-            showAnime();
-        }else{
-            document.querySelector(`#${el.dataset.genre}`).classList.remove('none') 
-        }
+        preparAnime(el.dataset.genre);
+     
     }
 });
 
-function showAnime(){
+function preparAnime(id){
     fetch(`https://api.jikan.moe/v3/genre/anime/${id}/1`).then((response) => {
         return response.json();
     }).then((category) =>{
         anime = category.anime; 
         console.log(anime);
-        var animeArray;
+        var animeArray = [];
         for (i = 0; i < 10; i++) {
-            animeArray = 
+            animeArray.push(
                 {
-                    i : 
-                    {
-                        'id' : anime[i].mal_id,
-                        'title' : anime[i].title,
-                        'genres' : anime[i].genres,
-                        'score' : anime[i].score,
-                        'synopsis' : anime[i].synopsis,
-                        'type' : anime[i].type,
-                        'episodes' : anime[i].episodes,
-                        'kids' : anime[i].kids,
-                        'r18' : anime[i].r18,
-                        'image_url' : anime[i].image_url
-                    }
-                }
+                    'id' : anime[i].mal_id,
+                    'title':  anime[i].title,
+                    'genres':  anime[i].genres,
+                    'score' : anime[i].score,
+                    'synopsis':  anime[i].synopsis,
+                    'type':  anime[i].type,
+                    'episodes':  anime[i].episodes,
+                    'kids':  anime[i].kids,
+                    'r18':  anime[i].r18,
+                    'image_url':  anime[i].image_url
+                });
         }
-        debugger;
-        console.log(animeArrays)
+        console.log(animeArray)
+        showAnime(animeArray)
+    }); 
+}
+function showAnime(animeArray){
+    for (i = 0; i < 1; i++) {
+        document.querySelector('.main').appendChild(Card.createCard());
         let imageDiv = document.querySelector('#image_url');
-        
         let image = document.createElement('img');
-        image.setAttribute("src", `${anime.image_url}`)
-        document.querySelector('#title').innerHTML = anime.title;
-        document.querySelector('#type').innerHTML = anime.type;
-        let genres = anime.genres;
+        image.setAttribute("src", `${animeArray[i].image_url}`)
+        document.querySelector('#title').innerHTML = animeArray[i].title;
+        document.querySelector('#type').innerHTML = animeArray[i].type;
+        let genres = animeArray[i].genres;
         genres.forEach(element => {
             allGenres += element.name+", ";
         });
         document.querySelector('#genres').innerHTML = allGenres;
-        document.querySelector('#episodes').innerHTML = anime.episodes;
+        document.querySelector('#episodes').innerHTML = animeArray[i].episodes+"ep";
+        document.querySelector('.high').innerHTML = animeArray[i].score;
+        document.querySelector('#synopsis').innerHTML = tronque_description(animeArray[i].synopsis, 65);
         
-        
-        document.querySelector('#synopsis').innerHTML = tronque_description(anime.synopsis, 65);
+
         imageDiv.appendChild(image);
-        
-    }); 
+    }
 }
